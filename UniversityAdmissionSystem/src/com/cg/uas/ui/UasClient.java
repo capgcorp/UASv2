14ea693266fc8d9a5d20b01e6d2e32e5572fe930
capgcorp <33022963+capgcorp@users.noreply.github.com>
2017-10-25 14:52:50 +0530

    Update UasClient.java
    More Ui functions added

--- UniversityAdmissionSystem/src/com/cg/uas/ui/UasClient.java
+++ UniversityAdmissionSystem/src/com/cg/uas/ui/UasClient.java
@@ -1,9 +1,11 @@
 package com.cg.uas.ui;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.PropertyConfigurator;
 
+import com.cg.uas.dto.Application;
 import com.cg.uas.dto.Programs_Scheduled;
 import com.cg.uas.exception.UasException;
 import com.cg.uas.service.IServiceUas;
@@ -24,12 +26,15 @@ public class UasClient {
 		}
 		PropertyConfigurator.configure("resources/log4j.properties");
 
+		/*List<Programs_Scheduled> programsList;*/
+		boolean verify;
+		String status;
 		List<Programs_Scheduled> programsList;
-		
-		
+		List<Application> applicationsList;
+		List<Application> applicantsList;
+		/*//------1. all programs-------------------//
 		try {
 			programsList =service.allPrograms();
-			System.out.println("try enter");
 			for(Programs_Scheduled program: programsList)
 			{	System.out.println("for enter");
 				System.out.println(program.toString());
@@ -37,6 +42,99 @@ public class UasClient {
 		} catch (UasException exp) {
 			System.err.println(exp.getMessage());
 		}
+		
+		//------------show status-----------------//
+		try {
+			status =service.status(10);
+		} catch (UasException exp) {
+			System.err.println(exp.getMessage());
+		}
+		
+		//------------accept application-------------//
+		try {
+			verify = service.acceptApplication(1, "27-oct-17");
+			System.out.println(verify);
+		} catch (UasException exp) {
+			System.err.println(exp.getMessage());
+		}
+		
+		
+		//------------reject application--------------//
+		try {
+			verify = service.rejectApplication(1);
+			System.out.println(verify);
+		} catch (UasException exp) {
+			System.err.println(exp.getMessage());
+		}
+		*/
+		
+		/*//--------------mac login-------------------//
+			try {
+				verify=service.macLogin("mac", "admin");
+				System.out.println(verify);
+			} catch (UasException exp) {
+				System.err.println(exp.getMessage());
+			}
+		*/
+		
+		/*//--------------admin login------------------//
+		try {
+			verify=service.adminLogin("admin", "admin");
+			System.out.println(verify);
+		} catch (UasException exp) {
+			System.err.println(exp.getMessage());
+		}*/
+		
+		/*//----------------confirm application------------//
+		try {
+			verify = service.confirmApplication(10);
+			System.out.println(verify);
+		} catch (UasException exp) {
+			System.err.println(exp.getMessage());
+		}
+		*/
+		
+		/*
+		//-------------program list in time period----------------//
+		try {
+			programsList = service.viewPrograms("19-oct-17", "31-oct-17");
+
+			for (Programs_Scheduled program : programsList) {
+				System.out.println(program.toString());
+			}
+		} catch (UasException exp) {
+			System.err.println(exp.getMessage());
+		}*/
+		
+		/*//------------all applications--------------------//
+		try {
+			applicationsList = service.allApplications("orcl");
+			for (Application application : applicationsList) {
+				System.out.println(application.toString());
+			}
+		} catch (UasException exp) {
+			System.err.println(exp.getMessage());
+		}
+		
+		//-----------all applications----------------//
+		try {
+			applicationsList = service.viewApplicants();
+			for (Application applicant : applicationsList) {
+				System.out.println(applicant.toString());
+			}
+		} catch (UasException exp) {
+			System.err.println(exp.getMessage());
+		}	*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 		
 		
 		
