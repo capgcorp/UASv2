e78413d2c0f7704a093c795a7c53bb00c81c6b78
capgcorp <33022963+capgcorp@users.noreply.github.com>
2017-10-25 12:42:02 +0530

    Update IDaoUas.java
    arguments updated as per iquery mapper

--- UniversityAdmissionSystem/src/com/cg/uas/dao/IDaoUas.java
+++ UniversityAdmissionSystem/src/com/cg/uas/dao/IDaoUas.java
@@ -21,11 +21,11 @@ public interface IDaoUas {
 	public boolean rejectApplication(int application_Id) throws UasException;
 	public boolean confirmApplication(int application_Id) throws UasException;
 	
-	public boolean updProgramOffered(Programs_Offered program_offered)  throws UasException;
+	public boolean updProgramOffered(Programs_Offered program_offered, String ProgramName)  throws UasException;
 	public boolean addProgramOffered(Programs_Offered program_offered)	throws UasException;
 	public boolean delProgramOffered(String ProgramName)  throws UasException;
 	
-	public boolean updProgramScheduled(Programs_Scheduled program_Scheduled)  throws UasException;
+	public boolean updProgramScheduled(Programs_Scheduled program_Scheduled, String scheduled_Program_Id)  throws UasException;
 	public boolean addProgramScheduled(Programs_Scheduled program_Scheduled)  throws UasException;
 	public boolean delProgramScheduled(String scheduled_Program_Id)  throws UasException;
 	
